a707654c206e5301afe9f159a35d71844e5caef0
Inge Saunders <ingesaunders@gmail.com>
2020-01-31 17:43:58 +0200

    Button states 1

--- schoolintercom.js
+++ schoolintercom.js
@@ -1,30 +1,83 @@
-//<a href="https://www.freepik.com/free-photos-vectors/business">Business vector created by kraphix - www.freepik.com</a>
-//<a href="https://www.freepik.com/free-photos-vectors/icon">Icon vector created by studiogstock - www.freepik.com</a>
-//<a href="https://www.freepik.com/free-photos-vectors/icon">Icon vector created by studiogstock - www.freepik.com</a>
-//<a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by freepik - www.freepik.com</a>
+const recordbutton = document.getElementById("record_play");
+recordbutton.innerText = recordbutton.textContent = 'Click to record';
 
-navigator.mediaDevices.getUserMedia ({ audio : true})
- .then(stream => {
-     const mediaRecorder = new MediaRecorder(stream);
-     mediaRecorder.start();
+const sendbutton = document.getElementById("send");
+sendbutton.innerText = sendbutton.textContent = 'Send to';
 
-     const audioChunks = [];
+const cancelbutton = document.getElementById("cancel");
+cancelbutton.innerText = cancelbutton.textContent = 'Cancel';
 
-     mediaRecorder.addEventListener("dataavailable", event => {
-         audioChunks.push(event.data);
-     });
+document.getElementById("send").disabled = true;
+document.getElementById("cancel").disabled = true;
+
+function onClick() {
+    var click = document.getElementById("record_play");
+    if (click.innerHTML === "Click to record") {
+        click.innerHTML = "Recording...";
+    } else {
+        click.innerHTML = "Play";
+    }
+}
 
 
-     mediaRecorder.addEventListener("stop", () => {
-         const audioBlob = new Blob(audioChunks);
-         const audioUrl = URL.createObjectURL(audioBlob);
-         const audio = new Audio (audioUrl);
-         audio.play();
 
-     });
+document.getElementById("send").disabled = false;
+    document.getElementById("cancel").disabled = false;
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+        const mediaRecorder = new MediaRecorder(stream);
+        //mediaRecorder.start();
 
-     setTimeout(() => {
-        mediaRecorder.stop();
-    }, 3000);
- });
+        const audioChunks = [];
+
+        mediaRecorder.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
+        });
+
+
+        mediaRecorder.addEventListener("stop", () => {
+            const audioBlob = new Blob(audioChunks);
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+
+        });
+
+        setTimeout(() => {
+            mediaRecorder.stop();
+        }, 3000);
+    });
+
+
+/*state: ready to record
+buttons:
+- record (next state is recording)
+ - enabled
+ - label Hold to record
+ - mouse down eventHandler
+- send disabled
+- cancel disabled
+
+state: recording
+buttons:
+- record: (next state is recorded)
+ - enabled
+ - mouse up event handlr
+   - creates next handlers
+   - set label to Play
+- send disabled
+- cancel disabled
+
+state: recorded
+buttons:
+- record_play: next state recorded
+ - label: play
+- send enabled  (next state ready to record)
+- cancel enabled (next state ready to record)
+-
+
+enter state funtion - ready to record (before audio recording code)
+*/
+
+
 
