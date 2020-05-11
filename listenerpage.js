5fac4c3fcec3328a531d273d335225b9c1236b88
inges <ingesaunders@gmail.com>
2020-05-11 20:41:17 +0200

    Download and play audio

--- listenerpage.js
+++ listenerpage.js
@@ -1,4 +1,4 @@
-   
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -11,14 +11,23 @@ function myFunction() {
 
 const playButton = document.getElementById("play_button");
 
-      playButton.innerText = playButton.textContent = 'No new messages';
+playButton.innerText = playButton.textContent = 'No new messages';
 
-$.get( "http://localhost:3000/tracks/new", function( tracks ) {
-   console.log(tracks);
-   tracks.forEach(track => { 
-     (new Audio()).play;
-   });
-   
+const api = "http://localhost:3000";
+
+$.get(api + "/tracks/new", function (tracks) {
+  
+  tracks.forEach(track => {
+    console.log(track);
+    var sound = document.createElement('audio');
+    sound.id = 'audio-player';
+    sound.controls = 'controls';
+    sound.src = api + track.url;
+    sound.type = 'audio/mpeg';
+    document.getElementById('player-container').appendChild(sound);
+    console.log(sound);
+  });
+
 });
 
 
