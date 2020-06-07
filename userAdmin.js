6427474696e5e5cf3f31d35843363d6daade3230
inges <ingesaunders@gmail.com>
2020-06-07 16:41:18 +0200

    POST request submit email

--- userAdmin.js
+++ userAdmin.js
@@ -12,30 +12,33 @@ $.get(api + "/users", function (users) {
         var userContainer = $(document.createElement('div'));
         userContainer.addClass("user-container");
         userContainer.text(user.emailAdress);
-    })
+
+        usersContainer.append(userContainer);
+    });
 });
 
 function createUserEvent(event) {
-    
+
     log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-    event.preventDefault();
-    
+
     const emailAddress = $("#email").val();
 
     $.ajax({
         type: 'POST',
         url: 'http://localhost:3000/users',
-        data: { emailAddress: emailAddress },
-        //processData: false,
-        //contentType: false
-    }).done(function (data) {
-        console.log(data);
-  });
-  
-  event.preventDefault();
+        data: JSON.stringify({ emailAddress: emailAddress }), 
+        dataType: 'json',
+        contentType: 'application/json',
+    }).done(function(data) {
+       console.log(data);
+    }).fail(function(xhr, status, error){
+       console.log("whoops", xhr, status, error);
+    });
+
+    event.preventDefault();
 };
-  
-  const form = document.getElementById('form');
-  const log = document.getElementById('log');
-  form.addEventListener('submit', createUserEvent);
+
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+form.addEventListener('submit', createUserEvent);
 
