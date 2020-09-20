6058f8bce10619e2468c53adfb2318a2fbc2ca81
inges <ingesaunders@gmail.com>
2020-09-20 17:24:36 +0200

    clickAway button added

--- listenerpage.js
+++ listenerpage.js
@@ -12,24 +12,20 @@ function myFunction() {
 //const playButton = document.getElementById("play_button");
 
 //playButton.innerText = playButton.textContent = 'No new messages';
-  localStorage.getItem("user-id");
-  console.log(localStorage.getItem("user-id"));
-  
-  function loggingIn() {
-    const userId = localStorage.getItem("user-id");
-    if ( userId !== null ) {
-    const logoutButton = document.getElementById("logout");
-            logoutButton.innerHTML = "Logout";
-    } else {
-      window.location = "/loginpage";
-    };
-  };
-  loggingIn();
-    
-   
+
+const userId = localStorage.getItem("user-id");
+console.log(userId);
+
+if (userId !== null) {
+  const logoutButton = document.getElementById("logout");
+  logoutButton.innerHTML = "Logout";
+} else {
+  window.location = "/loginpage";
+};
+
 const api = "http://localhost:3000";
 
-$.get(api + "/tracks/new", function (tracks) {
+$.get(api + `/users/${userId}/tracks/new`, function (tracks) {
   const tracksContainer = $("#tracks-container");
 
   tracks.forEach(track => {
@@ -43,11 +39,13 @@ $.get(api + "/tracks/new", function (tracks) {
     sound.controls = 'controls';
     sound.src = api + track.url;
     sound.type = 'audio/mpeg';
+    
 
     trackContainer.append(sound);
 
     var dateContainer = $(document.createElement('span'));
     dateContainer.addClass("date-container");
+    
     var uploadDate = Date.parse(track.uploadDate);
     var config = {
       weekday: 'long',
@@ -57,18 +55,25 @@ $.get(api + "/tracks/new", function (tracks) {
       hour: '2-digit',
       minute: '2-digit',
     };
-    
+
     const dateTimeFormat = new Intl.DateTimeFormat('en', config);
-    const [{ value: weekday }, ,{ value: month }, , { value: day }, , { value: year }, , { value: hour }, , { value: minute } ] = dateTimeFormat.formatToParts(uploadDate);
+    const [{ value: weekday }, , { value: month }, , { value: day }, , { value: year }, , { value: hour }, , { value: minute }] = dateTimeFormat.formatToParts(uploadDate);
 
     var dateString = `${weekday} ${day} ${month} ${year} ${hour}:${minute}`;
     dateContainer.text(dateString);
     trackContainer.append(dateContainer);
+    const clickAway = document.createElement('button');
+    $(clickAway).text('X');
+    $(clickAway).click((event)=>{
+      alert("here");
+      console.log(event);
+    });
+
+    trackContainer.append(clickAway);
 
     tracksContainer.append(trackContainer);
-
+    
     console.log(sound);
-
   });
 
 });
